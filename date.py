cde216567fe80d07db148b06c839d6ae488823ac
mohamedabcd <mohamed.abdennebi@gmail.com>
2022-09-18 22:31:21 +0200

    python script

---
+++ date.py
@@ -0,0 +1,9 @@
+# locale.setlocale(locale.LC_ALL, "fr_FR")
+# d = datetime.now()
+# print(d)
+# number_day = d.strftime("%d")
+# day = d.strftime("%A").capitalize()
+# month = d.strftime("%B").capitalize()
+# print(day, month,number_day)
+
+print("hello")
